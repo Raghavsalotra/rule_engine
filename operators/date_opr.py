815c0898041133808a0d6c74fb34b8de9567dd00
raghav <raghav.salotra@edgenetworks.in>
2018-06-28 18:32:12 +0530

    documentation for date operator functions added.

--- operators/date_opr.py
+++ operators/date_opr.py
@@ -1,7 +1,13 @@
 from time import strptime
 
 def less_than_equals(source, target):
-    src_date = source.replace('_', ' ')
+    """
+
+    :param source: [] list ['ATL9', 'greater_Than_equals', '2016-06-13 22:40:10']
+    :param target: {} {u'signal': u'ATL9', u'value_type': u'Datetime', u'value': u'2017-06-13 22:40:10'}
+    :return:
+    """
+    src_date = source[2].replace('_', ' ')
     src_date = strptime(src_date, '%Y-%m-%d %H:%M:%S')
     trg_date = strptime(target['value'], '%Y-%m-%d %H:%M:%S')
 
@@ -12,6 +18,12 @@ def less_than_equals(source, target):
 
 
 def greater_than_equals(source, target):
+    """
+
+        :param source: [] list ['ATL9', 'greater_Than_equals', '2016-06-13 22:40:10']
+        :param target: {} {u'signal': u'ATL9', u'value_type': u'Datetime', u'value': u'2017-06-13 22:40:10'}
+        :return:
+    """
 
     src_date = source[2].replace('_', ' ')
     src_date = strptime(src_date, '%Y-%m-%d %H:%M:%S')
